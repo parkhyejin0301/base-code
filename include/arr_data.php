a34f269850923e5691e364d0e0526b73af9f984f
parkhyejin0301 <78453708+parkhyejin0301@users.noreply.github.com>
2023-02-05 22:19:07 +0900

    base 셋팅

---
+++ include/arr_data.php
@@ -0,0 +1,91 @@
+<?
+
+$site_title = ""; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = ""; // 회사명
+
+$P_addr1 = ""; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "";
+$P_tel2 = "";
+
+$P_fax1 = "";
+$P_fax2 = "";
+
+$P_email1 = "";
+$P_email2 = "";
+
+$P_business_no = ""; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "브랜드소개";
+$nav_1_1 = "브랜드 스토리";
+$nav_1_2 = "BI";
+$nav_2 = "메뉴보기";
+$nav_2_1 = "매장·배달·포장";
+$nav_2_2 = "밀키트 배송";
+$nav_3 = "매장찾기";
+$nav_4 = "창업안내";
+$nav_4_1 = "기간혜택";
+$nav_4_2 = "점주인터뷰";
+$nav_4_3 = "브랜드강점";
+$nav_4_4 = "창업절차";
+$nav_4_5 = "창업비용";
+$nav_4_6 = "창업문의";
+$nav_5 = "더보기";
+$nav_5_1 = "공지사항";
+$nav_5_2 = "이벤트";
+$nav_5_3 = "FAQ";
+$nav_6 = "회사소개";
+$nav_6_1 = "회사소개";
+$nav_6_2 = "고객센터";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../about/brand.html";
+$nav_1_1_href = "../about/brand.html";
+$nav_1_2_href = "../about/brand.html#bi-section";
+$nav_2_href = "../menu/list.html";
+$nav_2_1_href = "../menu/list.html";
+$nav_2_1_1_href = "../menu/view.html";
+$nav_2_2_href = "../menu/list2.html";
+$nav_3_href = "../store/find.html";
+$nav_4_href = "../foundation/info.html";
+$nav_4_1_href = "../foundation/info.html";
+$nav_4_2_href = "../foundation/info.html#interview-section";
+$nav_4_3_href = "../foundation/info.html#strong-section";
+$nav_4_4_href = "../foundation/info.html#open-sequence";
+$nav_4_5_href = "../foundation/info.html#open-cost";
+$nav_4_6_href = "../foundation/info.html#chain-store";
+$nav_5_href = "../more/notice.html";
+$nav_5_1_href = "../more/notice.html";
+$nav_5_1_1_href = "../more/notice-view.html";
+$nav_5_2_href = "../more/event.html";
+$nav_5_2_1_href = "../more/event-view.html";
+$nav_5_3_href = "../more/cs.html";
+$nav_6_href = "../foot/company.html";
+$nav_6_1_href = "../foot/company.html";
+$nav_6_2_href = "../foot/cs-center.html";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+?>
