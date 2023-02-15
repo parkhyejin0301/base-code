5be16b52d89442daff21f77bbde81b07a4982768
parkhyejin0301 <78453708+parkhyejin0301@users.noreply.github.com>
2023-02-15 21:18:01 +0900

    베이스 셋팅

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,15 +1,5 @@
 'use strict';
 
-/* 
-  퍼블팀 js 파일입니다. 
-  
-  ※ 퍼블팀 외에 수정금지
-  개발에서 필요한 부분은 dev.js 파일에 작업 부탁드리며, 
-  common.js에서 삭제 필요한 부분은 퍼블팀에 따로 전달 부탁드립니다.
-  
-*/
-
-
 /* 모달팝업 */
 var startPop = function() {
   var winW = $(window).width();
@@ -24,112 +14,6 @@ var startPop = function() {
   });
   }
 }
-
-/* loading image */
-$(window).on('load', function(){
-  $('.loading-image').fadeOut(300);
-}); 
-
-
-/* GNB */
-/*
-var gnbdrop = {
-  init: function () {
-    // this.gnbdrop_all(); //모든 2depth+배경 노출
-    // this.gnbdrop_box(); //호버한 1depth>link의 2depth 박스형태로 노출
-    // this.gnbdrop_line(); //호버한 1depth>link의 2depth + 배경이 노출
-  },
-  gnbdrop_all: function () {
-    //모든 2depth+배경 노출
-    var $nav_bg = $('.nav__bg'),
-    // $header = $('.header'),
-      $nav_link = $('.header .nav .depth-1').children('.link'),
-      $nav_drop = $('.header .nav .nav-list--depth2');
-
-      $('.header .nav').each(function () {
-        var maxHeight = 0;
-
-        $(this)
-        .find('.nav-list--depth2')
-        .each(function () {
-          if ($(this).outerHeight() > maxHeight) {
-            maxHeight = $(this).outerHeight();
-          }
-        });
-
-        $nav_bg.height(maxHeight);
-
-        $(this)
-        .find($nav_link)
-        .each(function (index) {
-          $(this).on('mouseover focus', function () {
-            // $header.addClass('hover');
-            $nav_bg.stop(false,true).fadeIn(200,"linear"); //slideDown(200);
-            $nav_drop.stop(false,true).fadeIn(200,"linear"); //slideDown(200);
-          });
-        });
-
-      $('.header').on('mouseleave', function () {
-        // $header.removeClass('hover');
-        $nav_drop.stop(false,true).fadeOut(200,"linear"); //slideUp(200);
-        $nav_bg.stop(false,true).fadeOut(200,"linear"); //slideUp(200);
-        });
-      });
-  },
-
-  gnbdrop_box: function () {
-    //호버한 1depth>link의 2depth 박스형태로 노출
-    var $nav_link = $('.header .nav .depth-1').children('.link'),
-      $nav_drop = $('.header .nav .nav-list--depth2');
-    $('.header .nav').each(function () {
-      $(this)
-        .find($nav_link)
-        .each(function (index) {
-          $(this).on('mouseover focus', function () {
-            if ($(this).next().length > 0) {
-              $nav_drop.stop(false, true).fadeOut(100);//slideUp();
-              $(this).next().stop(false, true).fadeIn(100);//slideDown();
-            } else {
-              $nav_drop.stop(false, true).fadeOut(100);//slideUp();
-            }
-          });
-        });
-      $(this).on('mouseleave', function () {
-        $nav_drop.stop(false, true).fadeOut(100);//slideUp();
-      });
-    });
-  },
-
-  gnbdrop_line: function () {
-    //호버한 1depth>link의 2depth + 배경이 노출
-    var $nav_bg = $('.nav__bg'),
-      $nav_link = $('.depth-1').children('.link'),
-      $nav_drop = $('.nav-list--depth2');
-    $('.header .nav').each(function () {
-      var $nav_bg_height = $nav_drop.innerHeight();
-      $nav_bg.height($nav_bg_height);
-      $(this)
-        .find($nav_link)
-        .each(function (index) {
-          $(this).on('mouseover focus', function () {
-            if ($(this).next().length > 0) {
-              $nav_drop.stop(false, true).hide();
-              $nav_bg.stop(false, true).show();
-              $(this).next().stop(false, true).css('display','flex');//show();
-            } else {
-              $nav_drop.stop(false, true).hide();
-              $nav_bg.stop(false, true).hide();
-            }
-          });
-        });
-      $(this).on('mouseleave', function () {
-        $nav_drop.stop(false, true).hide();
-        $nav_bg.stop(false, true).hide();
-      });
-    });
-  },
-};
-*/
 
 /* 모바일 네비게이션 */
 var navMobile = {
@@ -284,38 +168,7 @@ var subNav = {
   },
 };
 
-/* 서브네비게이션 스크롤 형태 */
-var subNavScroll = {
-  init: function () {
-    this.sticky_nav();
-  },
 
-  sticky_nav: function () {
-    var $subNav = $('.sub-nav--sticky'),
-      $subNavWrap = $('.sub-nav--sticky .sub-nav__wrap');
-    if ($subNav.length > 0) {
-      var subNavOffset = $subNav.offset(),
-        // subNavTop = subNavOffset.top;
-        subNavTop = subNavOffset.top - $('.header').outerHeight(); // #헤더 고정형일때
-
-      if ($(window).outerWidth() > 768) {
-        if ($(window).scrollTop() > subNavTop) {
-          $subNav.addClass('fix');
-          $subNavWrap.css('top', $('.header').outerHeight()); //#헤더 고정형일때
-        } else {
-          $subNav.removeClass('fix');
-          $subNavWrap.css('top', 0); //#헤더 고정형일때
-        }
-      } else {
-        if ($(window).scrollTop() > subNavTop) {
-          $subNav.addClass('fix');
-        } else {
-          $subNav.removeClass('fix');
-        }
-      }
-    }
-  },
-};
 
 /* Magnific 팝업 */
 var magnificPop = {
@@ -406,23 +259,7 @@ $(document).ready(function () {
     callback: function () {}, // 콜백 함수
   });
 
-  /* 
-    이미지맵 제작 추천 : https://www.image-map.net/
-    이미지맵 반응형 작업 시 선언
-    $('img[usemap]').rwdImageMaps();
-  */
 
-  /* 
-  $('[data-track]').scrollTrack({
-    threshold: 0, // // 임계치 ( * 상단 고정영역 높이 )
-    activeClass: 'active', // 활성 클래스 네이밍 (기본값 : active)
-  });
-  */
-
-    /**
- * qna
- * 아코디언
- */
  var qnaFun = {
   init: function () {
     this.q();
@@ -457,13 +294,9 @@ $(document).ready(function () {
     activeClass: 'active',
   });
  
-
-  //gnbdrop.init();
   navMobile.init();
-  subNav.init();
-  subNavScroll.init();
   startPop();
-  qnaFun.init(); //qna
+  qnaFun.init();
 
   var wowJS = new WOW().init(); 
 
@@ -510,116 +343,6 @@ $(wowrap).each(function () {
     }
   });
 
-  /* scrollbar js */
-  $('.scrollbar-inner').scrollbar();
-  // $('.scrollbar-outer').scrollbar();
-  // $('.scrollbar-light').scrollbar();
-
-  /* Resizing Check */
-  var resizing = resizingCheck({
-    breakpoints: {
-      414: function () {
-        // console.log('414 < width < 768');
-      },
-      768: function () {
-        // console.log('768 < width < 1024');
-      },
-      1024: function () {
-        // console.log('1024 < width < 1200');
-      },
-      1200: function () {
-        // console.log('1200 < width');
-      },
-    },
-  });
 });
 
 
-
-/* File Upload */
-$(document).on('change', '.file-box input[type="file"]', function () {
-  var tmp = $(this).val().replace(/^.*\\/, "");
-  $(this).parents('.file-box').find('.file-box__text').text(tmp);
-  $(this).parents('.file-box').find('.file-box__text').addClass('on');
-});
-
-/* File Upload - 구버전 */
-$(document).on('change', '.file_box input[type="file"]', function () {
-  var tmp = $(this).val().replace(/^.*\\/, "");
-  $(this).siblings("p").text(tmp);
-});
-
-//suv-nav 스크롤
-function subNavScroll_front() {
-  var $subNav = $('.sub-nav__wrap');
-  var subNavLinkLeft = $('.sub-nav__wrap .link.on').offset().left;
-  var subNavWidth = $('.sub-nav__wrap');
-  var totalWidth = 0;
-
-  for (var i = 0; i < subNavWidth.length; i++) {
-    totalWidth += subNavWidth[i].offsetWidth;
-  }
-
-  $subNav.animate({ scrollLeft: subNavLinkLeft - 100 }, 1);
-}
-
-
-$(document).ready(function () {
-  if ($('.sub-nav__wrap').length > 0) {
-    if ($(window).innerWidth() < 1201) {
-      subNavScroll_front();
-    }
-  }
-});
-$(window).resize(function () {
-  if ($('.sub-nav__wrap').length > 0) {
-    if ($(window).innerWidth() < 1201) {
-      subNavScroll_front();
-    }
-  }
-});
-
-
-/* 본인인증 팝업 내 이용약관 보기 open/hide */
-$('.terms-chk__head .icon').on('click', function () {
-  $(this).parents('.terms-chk').find('.terms-chk__body').stop().slideToggle(200);
-  $(this).toggleClass('active');
-});
-
-$('.terms-text-open').on('click', function () {
-  if ($(this).parents('.terms-tr').find('.terms-text').css('display') == 'none') {
-    $(this).text('닫기');
-    $(this).parents('.terms-tr').find('.terms-text').stop().slideDown(200);
-  } else {
-    $(this).text('전문보기');
-    $(this).parents('.terms-tr').find('.terms-text').stop().slideUp(200);
-  }
-});
-
-$(document).ready(function () {
-  // 아이디 유효성 체크 및 중복 검사
-
-  $(document).on('click', '#all_agree', function () {
-    if ($(this).is(':checked')) $('.agree_forms').prop('checked', true);
-    else $('.agree_forms').prop('checked', false);
-  });
-
-  $(document).on('click', '.agree_forms', function () {
-    if ($(this).is(':checked')) {
-
-      var total = $('.terms-chk input[type=checkbox]').not('#all_agree').length;
-      var checked = $('.terms-chk input[type=checkbox]:checked').not('#all_agree').length;
-      if (total == checked) $('#all_agree').prop('checked', true);
-    }
-    else $('#all_agree').prop('checked', false);
-  });
-
-});
-
-//input 폼 검증 실패일때 invalid
-document.querySelectorAll("input").forEach(input => {
-  input.addEventListener("invalid", () => {
-    // 검증 후 폼 요소에 was-validated 클래스로 표시해 둔다
-    document.forms[0].classList.add("was-validated")
-  })
-})
